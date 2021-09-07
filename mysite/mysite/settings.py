b441847d4014eea94d0faa0c59af07c51c526a8f
Lance L <lance@Lances-MacBook-Air.local>
2021-09-06 17:06:20 -0700

    part5 testing

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polls',
-        'USER': 'lawnknee',
+        'USER': 'lance',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
