eb74beb9ca8b9e9c8f1704164158ef5863433248
Loni <kuangloni@gmail.com>
2021-09-07 14:26:38 -0500

    completed step 6 and 7. added search bar, filters, and question template order and choices

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polls',
-        'USER': 'lance',
+        'USER': 'lawnknee',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
